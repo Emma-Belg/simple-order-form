4584be6dd970db009804d91251beab415dbfaee6
emma <emma.wishart@live.com.au>
2020-05-14 14:18:34 +0200

    redoing simple order form task

---
+++ form-view.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css"
+          rel="stylesheet"/>
+    <title>Order food & drinks</title>
+</head>
+<body>
+<div class="container">
+    <h1>Order food in restaurant "the Personal Ham Processors"</h1>
+    <nav>
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="?food=1">Order food</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?food=0">Order drinks</a>
+            </li>
+        </ul>
+    </nav>
+    <form method="post">
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <?php
+                email();
+                isRequired("email");
+                ?>
+                <label for="email">E-mail:</label>
+                <input type="text" id="email" name="email"
+                       value = "<?php sessionData("email"); ?>"
+                       class="form-control"/>
+            </div>
+            <div></div>
+        </div>
+
+        <fieldset>
+            <legend>Address</legend>
+
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <?php
+                    isRequired("street");
+                    ?>
+                    <label for="street">Street:</label>
+                    <input type="text" name="street" id="street" class="form-control"
+                           value = "<?php sessionData("street"); ?>">
+                </div>
+                <div class="form-group col-md-6">
+                    <?php
+                    isRequired("streetnumber");
+                    numberOnly("streetnumber");
+                    ?>
+                    <label for="streetnumber">Street number:</label>
+                    <input type="text" id="streetnumber" name="streetnumber" class="form-control"
+                           value = "<?php sessionData("streetnumber"); ?>">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <?php
+                    isRequired("city");
+                    ?>
+                    <label for="city">City:</label>
+                    <input type="text" id="city" name="city" class="form-control"
+                           value = "<?php sessionData("city"); ?>">
+                </div>
+                <div class="form-group col-md-6">
+                    <?php
+                    isRequired("zipcode");
+                    numberOnly("streetnumber");
+                    ?>
+                    <label for="zipcode">Zipcode</label>
+                    <input type="text" id="zipcode" name="zipcode" class="form-control"
+                           value = "<?php sessionData("zipcode"); ?>">
+                </div>
+            </div>
+        </fieldset>
+
+        <fieldset>
+            <legend>Products</legend>
+            <?php foreach ($products AS $i => $product): ?>
+                <label>
+                    <input type="checkbox" value="1" name="products[<?php echo $i ?>]"/> <?php echo $product['name'] ?> -
+                    &euro; <?php echo number_format($product['price'], 2) ?></label><br />
+            <?php endforeach; ?>
+        </fieldset>
+
+        <button type="submit" class="btn btn-primary">Order!</button>
+    </form>
+
+    <footer>You already ordered <strong>&euro; <?php echo $totalValue ?></strong> in food and drinks.</footer>
+</div>
+
+<style>
+    footer {
+        text-align: center;
+    }
+</style>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,95 @@
+<?php
+declare(strict_types=1);
+
+//we are going to use session variables so we need to enable sessions
+session_start();
+
+$email = "";
+
+function check_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+function isRequired($data)
+{
+    if (empty($_POST[$data])) {
+        echo("<div class=\"alert alert-danger\">This field is required!</div>");
+    }
+}
+
+//ths is the thing
+$dataCorrect = false;
+
+function email()
+{
+    if (isset($_POST["email"])) {
+        $email = check_input($_POST["email"]);
+        // check if e-mail address is well-formed
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo $emailErr = "<div class=\"alert alert-danger\">invalid email format</div>";
+        } else {
+            //not seen here
+            $GLOBALS["dataCorrect"] = true;
+            echo $thankYou = "<div class=\"alert alert-success\">Thank you</div>";
+        }
+    }
+}
+
+function numberOnly($data)
+{
+    $number = $_POST[$data];
+
+    if (isset($number)) {
+        if (is_numeric($number)) {
+            $GLOBALS["dataCorrect"] = true;
+            echo "<div class=\"alert alert - success\">Thank you</div>";
+        } else {
+            echo "<div class=\"alert alert-danger\">Please enter only numbers.</div>";
+        }
+    }
+}
+
+function sessionData($data){
+    if($GLOBALS["dataCorrect"] == true){
+        $_SESSION[$data] = $_POST[$data];
+        echo $_SESSION[$data];
+    } else {
+        $_SESSION[$data] = "";
+    }
+}
+
+function whatIsHappening()
+{
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+}
+
+//your products with their price.
+$products = [
+    ['name' => 'Club Ham', 'price' => 3.20],
+    ['name' => 'Club Cheese', 'price' => 3],
+    ['name' => 'Club Cheese & Ham', 'price' => 4],
+    ['name' => 'Club Chicken', 'price' => 4],
+    ['name' => 'Club Salmon', 'price' => 5]
+];
+
+$products = [
+    ['name' => 'Cola', 'price' => 2],
+    ['name' => 'Fanta', 'price' => 2],
+    ['name' => 'Sprite', 'price' => 2],
+    ['name' => 'Ice-tea', 'price' => 3],
+];
+
+$totalValue = 0;
+
+require 'form-view.php';
